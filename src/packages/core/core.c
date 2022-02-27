4ebe6acff82f3b7d7abc897a604ce9d56afed692
Füchs <68223337+Fuechs@users.noreply.github.com>
2022-02-27 17:44:42 +0100

    Update core.c

--- src/packages/core/core.c
+++ src/packages/core/core.c
@@ -1,1 +1,14 @@
+#include "core.h"
 
+char* user_input(char* message) {
+
+  char* input;
+  char* input_msg;
+  
+  strcpy(input_msg, "%s%s", message);
+  
+  scanf(input_msg, &input);
+  
+  return input;
+
+}
