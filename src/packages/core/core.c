ad8b24742f1d374e2c254643f89d1b0846454b48
Füchs <68223337+Fuechs@users.noreply.github.com>
2022-02-28 10:02:15 +0100

    Update core.c

--- src/packages/core/core.c
+++ src/packages/core/core.c
@@ -1,4 +1,25 @@
-#include "core.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+char* main(char** argv) {
+
+  if (strcmp("user_input", argv[1]) == 0) {
+  
+    return user_input(argv[2]);
+    
+  }
+  
+  if (strcmp("lower_string", argv[1]) == 0 {
+  
+    return NULL;
+    
+  }
+  
+  return NULL;
+  
+}
 
 char* user_input(char* message) {
 
@@ -11,4 +32,16 @@ char* user_input(char* message) {
   
   return input;
 
+}
+      
+char* lower_string(char* string) {
+
+  return NULL;
+
+}
+      
+char* upper_string(char* string) {
+
+  return NULL;
+  
 }
