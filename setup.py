f2dd70a0a8b40fb763124b40b94d47f873f0917b
Füchs <68223337+Fuechs@users.noreply.github.com>
2022-03-11 12:53:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+##########################################################################################
+#
+#           Easy Setup for fux compiler
+#
+#           Just run this python script and answer the questions
+#
+##########################################################################################
+#
+#           Copyright (c) 2020-2022 Fuechs
+#           All rights reserved.
+#
+##########################################################################################
+
+import os
+clear = lambda: os.system("clear"); clear()
+
+
+def main():
+  print("soonTM")
+  return 0
+
+
+if __file__ == '__main__':
+  main()
