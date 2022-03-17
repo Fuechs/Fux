b80592796e4e7859721a5db0cfa652a58dc6abf5
Füchs <68223337+Fuechs@users.noreply.github.com>
2022-03-17 11:36:07 +0100

    Update core.c

--- src/packages/core/core.c
+++ src/packages/core/core.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-
-int main() {
-  return 0;
-}
-
 int output(char* message) {
   if (!message) return 1;
   print("%s", message);
   return 0;
+}
+
+char* input() {
+  char* read[2048];
+  fread("%s", &read);
+  if (!read) return NULL;
+  return read;
+}
+
+
+
+int main(char** argv) {
+  return execute_command(argv[2]);
 }
