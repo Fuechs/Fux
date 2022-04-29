d4f28f8f936a3ad222ee098314d6df94dd493fa0
Fuechs <fuechsss@gmail.com>
2022-04-29 15:09:44 +0200

    web

--- docs/guide.py
+++ docs/guide.py
@@ -1,106 +1,30 @@
-import os, sys, time
-# from progress.bar import ChargingBar
+from rich.panel import Panel
 
-platforms = ["linux", "linux2", "darwin"]
-
-if sys.platform in platforms:
-    clear = lambda: os.system("clear")
-elif sys.platform == "win32":
-    clear = lambda: os.system("cls")
-
-clear()
+from textual.app import App
+from textual.reactive import Reactive
+from textual.widget import Widget
 
 
+class Hover(Widget):
 
-def guide(part: str = '0'):
+    mouse_over = Reactive(False)
 
-    if part == '0':
-        print("Introduction...")
-    
-    elif part.startswith('I'):
-        print("Part 1...")
+    def render(self) -> Panel:
+        return Panel("Hello [b]World[/b]", style=("on red" if self.mouse_over else ""))
 
-    time.sleep(0.5)
+    def on_enter(self) -> None:
+        self.mouse_over = True
 
-
-
-def menu():
-
-    run = True
-
-    title = "Full Guide for Fux"
-    head = len(title)
-    subtitle = "Flawless unambigous programming"
-    bottom = len(subtitle)
-
-    while run:
-
-        clear()
-
-        print('-'*head)
-        print(title)
-
-        print("1 - Start guide")
-
-        print("2 - Contents")
-
-        print("3 - Exit")
-
-        print(subtitle)
-        print('-'*bottom)
-
-        try: 
-            option = int(input(">> "))
-
-        except:
-            print("Input has to be an integer.")
-            time.sleep(0.65)
-            continue
-
-        match option:
-
-            case 1: 
-                guide()
-            case 2: 
-
-                title = "Contents"
-                head = len(title)
-                _run = True
-
-                while _run:
-
-                    clear()
-                    print('-'*head)
-                    print(title)
-
-                    print("0 - Getting started")
-                    print("I - Input and Output")
-
-                    print(subtitle)
-                    print('-'*bottom)
-
-                    option = input(">> ")
-
-                    match option:
-
-                        case '0': guide()
-                        case 'I': guide('I')
-
-                        case _: print("Unknown part"); time.sleep(0.65); _run = False
-
-                    _run = False
-
-            case 3: 
-                run = False
-            case _: 
-                print("Unknown option")
-                time.sleep(0.65)
-                continue
-
-    clear()
-    print("Thanks for using this guide.")
-    time.sleep(0.65)
-    exit()
+    def on_leave(self) -> None:
+        self.mouse_over = False
 
 
-menu()
+class HoverApp(App):
+    """Demonstrates custom widgets"""
+
+    async def on_mount(self) -> None:
+        hovers = (Hover() for _ in range(10))
+        await self.view.dock(*hovers, edge="top")
+
+
+HoverApp.run(log="textual.log")
