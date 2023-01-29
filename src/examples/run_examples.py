675230026227233b445d74e0d38585f1136e0a0e
Füchs <68223337+Fuechs@users.noreply.github.com>
2023-01-29 14:44:22 +0100

    add timer in run_examples.py

--- src/examples/run_examples.py
+++ src/examples/run_examples.py
@@ -1,9 +1,12 @@
 if __name__ == "__main__":
     from os import system, listdir
+    from time import time
 
     exceptions = []
     files: list[str] = [file for file in listdir("src/examples/") if file.endswith(".fux") and file not in exceptions]
+    start = time()
     for file in files:
         print("Running:", file)
         system(f"time ./fux src/examples/{file}")
         print("\033[31m"+'-'*60+"\033[39m")
+    print(f"Time: {time() - start}s")
